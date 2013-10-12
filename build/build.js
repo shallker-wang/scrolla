d81930e32d1802bf5f6e08f9e4b2470f0dad96a3
Shallker Wang <shallker.wang@profero.com>
2013-10-12 21:18:49 +0800

    U

--- build/build.js
+++ build/build.js
@@ -10312,39 +10312,125 @@ module.exports = function Content(el) {
       }
     });
 
-    $(this.el).on('mousewheel DOMMouseScroll', onMousewheel);
+    var wheel = 'onwheel' in document.createElement('div') ? 'wheel' : false;
+    var mousewheel = 'onmousewheel' in document.createElement('div') ? 'mousewheel' : false;
+    var mousescroll = 'DOMMouseScroll';
+
+    if (wheel) {
+      $(this.el).on(wheel, onWheel);
+    } else if (mousewheel) {
+      $(this.el).on(mousewheel, onMousewheel);
+    } else {
+      $(this.el).on(mousescroll, onMousescroll);
+    }
+
     return this;
   }.call(eventy({}));
 
-  function onMousewheel(mousewheel) {
-    var ev = mousewheel.originalEvent;
-    var delta = ev.detail ? -ev.detail * 10 : ev.wheelDelta;
+  function onWheel(ev) {
+    var wheel = ev.originalEvent;
 
-    if (delta > 0) {
-      scrollUp(delta);
-    } else {
-      scrollDown(-delta);
+    var wheelEvent = {
+      deltaX: -wheel.deltaX,
+      deltaY: -wheel.deltaY
     }
 
-    function scrollUp(distance) {
-      if (content.percentY <= 0) return;
-      if (content.scrollTop <= 0) return;
-      mousewheel.preventDefault();
+    onScroll.call(ev, wheelEvent);
+  }
 
-      content.scrollTop = content.scrollTop - distance;
-      content.trigger('scroll-up', distance);
-      content.trigger('scrolling').trigger('scrolling-y');
+  function onMousewheel(ev) {
+    var mousewheel = ev.originalEvent;
+
+    mousewheel.deltaX = mousewheel.wheelDeltaX;
+    mousewheel.deltaY = mousewheel.wheelDeltaY;
+    onScroll.call(ev, mousewheel);
+  }
+
+  function onMousescroll(ev) {
+    var mousescroll = ev.originalEvent;
+
+    mousescroll.delta = mousescroll.detail * -10;
+
+    // scroll horizontally
+    if (mousescroll.axis === 1) {
+      mousescroll.deltaX = mousescroll.delta;
+      mousescroll.deltaY = 0;
     }
 
-    function scrollDown(distance) {
-      if (content.percentY >= 100) return;
-      if (content.scrollTop >= content.availableScrollHeight) return;
-      mousewheel.preventDefault();
-
-      content.scrollTop = content.scrollTop + distance;
-      content.trigger('scroll-down', distance);
-      content.trigger('scrolling').trigger('scrolling-y');
+    // scroll vertically
+    if (mousescroll.axis === 2) {
+      mousescroll.deltaX = 0;
+      mousescroll.deltaY = mousescroll.delta;
     }
+
+    onScroll.call(ev, mousescroll);
+  }
+
+  function onScroll(scroll) {
+    if (typeof scroll.deltaX !== 'undefined') {
+      scrollX.call(this, scroll.deltaX);
+      scrollY.call(this, scroll.deltaY);
+    } else if (typeof scroll.wheelDeltaX !== 'undefined') {
+      scrollX.call(this, scroll.wheelDeltaX);
+      scrollY.call(this, scroll.wheelDeltaY);
+    } else if (typeof scroll.delta !== 'undefined') {
+      scrollY.call(this, scroll.delta);
+    } else if (typeof scroll.wheelDelta !== 'undefined') {
+      scrollY.call(this, scroll.wheelDelta);
+    } else if (typeof scroll.detail !== 'undefined') {
+      if (scroll.axis === 1) scrollX.call(this, scroll.detail * -10);
+      if (scroll.axis === 2) scrollY.call(this, scroll.detail * -10);
+    }
+  }
+
+  function scrollX(distance) {
+    if (distance > 0) scrollLeft.call(this, distance);
+    if (distance < 0) scrollRight.call(this, -distance);
+  }
+
+  function scrollY(distance) {
+    if (distance > 0) scrollUp.call(this, distance);
+    if (distance < 0) scrollDown.call(this, -distance);
+  }
+
+  function scrollUp(distance) {
+    if (content.percentY <= 0) return;
+    if (content.scrollTop <= 0) return;
+    this.preventDefault();
+
+    content.scrollTop = content.scrollTop - distance;
+    content.trigger('scroll-up', distance);
+    content.trigger('scrolling').trigger('scrolling-y');
+  }
+
+  function scrollDown(distance) {
+    if (content.percentY >= 100) return;
+    if (content.scrollTop >= content.availableScrollHeight) return;
+    this.preventDefault();
+
+    content.scrollTop = content.scrollTop + distance;
+    content.trigger('scroll-down', distance);
+    content.trigger('scrolling').trigger('scrolling-y');
+  }
+
+  function scrollLeft(distance) {
+    if (content.percentX <= 0) return;
+    if (content.scrollLeft <= 0) return;
+    this.preventDefault();
+
+    content.scrollLeft = content.scrollLeft - distance;
+    content.trigger('scroll-left', distance);
+    content.trigger('scrolling').trigger('scrolling-x');
+  }
+
+  function scrollRight(distance) {
+    if (content.percentX >= 100) return;
+    if (content.scrollLeft >= content.availableScrollWidth) return;
+    this.preventDefault();
+
+    content.scrollLeft = content.scrollLeft + distance;
+    content.trigger('scroll-right', distance);
+    content.trigger('scrolling').trigger('scrolling-x');
   }
 
   /*
